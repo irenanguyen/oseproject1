dbdcf42434e53fa17005b935be0fbf5e18fed175
irenanguyen <irenanguyen2002@seznam.cz>
2022-10-20 21:25:39 +0200

    task1

--- script.R
+++ script.R
@@ -1,6 +1,6 @@
 library(dplyr)
 head(neededdata)
-#1 I.	Zjistěte kolik škol, je v každém kraji
+#1 task 1 Zjistěte kolik škol, je v každém kraji
 neededdata %>%
   group_by(kraj) %>%
   summarise(totalschool = n()) %>%
