632083e238970cac66455cb003dd99f0dd686aca
irenanguyen <irenanguyen2002@seznam.cz>
2022-10-21 21:02:43 +0200

    změnit sloupec kraj na faktor dle pořadí NUTS3 (asi???)

--- script.R
+++ script.R
@@ -1,9 +1,15 @@
 library(dplyr)
 head(neededdata)
 #task 1 Zjistěte kolik škol, je v každém kraji
-neededdata %>%
+
+task_one <- neededdata %>%
   group_by(kraj) %>%
   summarise(totalschool = n()) %>%
   arrange(totalschool)
+task_one$kraj <- factor(task_one$kraj, levels = c("Praha", "Středočeský", "Jihočeský", "Plzeňský", "Karlovarský", "Ústecký", "Liberecký","Královéhradecký", "Pardubický", "Vysočina","Jihomoravský", "Olomoucký", "Zlínský", "Moravskoslezský"))
+
+
+
+
 
 
