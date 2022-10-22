b480b840a144c90b1b1d174677992c2d7d0dc18e
irenanguyen <irenanguyen2002@seznam.cz>
2022-10-22 12:10:34 +0200

    task 3 přiřadit školy do správných NUTS2 (??)

--- script.R
+++ script.R
@@ -6,10 +6,16 @@ task_one <- neededdata %>%
   group_by(kraj) %>%
   summarise(totalschool = n()) %>%
   arrange(totalschool)
+#task 2
 task_one$kraj <- factor(task_one$kraj, levels = c("Praha", "Středočeský", "Jihočeský", "Plzeňský", "Karlovarský", "Ústecký", "Liberecký","Královéhradecký", "Pardubický", "Vysočina","Jihomoravský", "Olomoucký", "Zlínský", "Moravskoslezský"))
 
-
-
-
-
+mutate(neededdata, NUTS2 = case_when(neededdata$kraj == "Praha" ~ "Praha", 
+                                     neededdata$kraj =="Středočeský" ~ "Střední Čechy",
+                                     neededdata$kraj =="Jihočeský" | neededdata$kraj =="Plzeňský" ~ "Jihozápad",
+                                     neededdata$kraj =="Karlovarský" | neededdata$kraj =="Ústecký" ~ "Severozápad",
+                                     neededdata$kraj =="Olomoucký" | neededdata$kraj =="Zlínský" ~ "Střední Morava",
+                                     neededdata$kraj =="Vysočina" | neededdata$kraj =="Jihomoravský" ~ "Jihovýchod",
+                                     neededdata$kraj =="Liberecký" | neededdata$kraj =="Královéhradecký" | neededdata$kraj =="Pardubický" ~ "Severovýchod",
+                                     neededdata$kraj == "Moravskoslezský" ~ "Moravskoslezsko",TRUE ~ "else"))
+print(neededdata)
 
