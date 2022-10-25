d0dc1c34d3115f9d926775dfb2cdcdc1cde8b761
irenanguyen <irenanguyen2002@seznam.cz>
2022-10-25 20:37:05 +0200

    visualisation task 4

--- script.R
+++ script.R
@@ -1,5 +1,6 @@
 library(dplyr)
 head(neededdata)
+#ggplot2, forcat, dplyr
 #task 1 Zjistěte kolik škol, je v každém kraji
 
 task_one <- neededdata %>%
@@ -19,7 +20,7 @@ neededdata <- mutate(neededdata, NUTS2 = case_when(neededdata$kraj == "Praha" ~ 
                                      neededdata$kraj =="Olomoucký" | neededdata$kraj =="Zlínský" ~ "Střední Morava",
                                      neededdata$kraj =="Vysočina" | neededdata$kraj =="Jihomoravský" ~ "Jihovýchod",
                                      neededdata$kraj =="Liberecký" | neededdata$kraj =="Královéhradecký" | neededdata$kraj =="Pardubický" ~ "Severovýchod",
-                                     neededdata$kraj == "Moravskoslezský" ~ "Moravskoslezsko",TRUE ~ "N/A"))
+                                     neededdata$kraj == "Moravskoslezský" ~ "Moravskoslezsko"))
 print(neededdata)
 View(task_one)
 
@@ -28,7 +29,15 @@ View(task_one)
 pocet_zaku_za_kraj <- aggregate(neededdata$pocet_zaku ~ neededdata$kraj, data = neededdata, FUN = sum)
 colnames(pocet_zaku_za_kraj) <- c("Kraj", "Pocet zaku")
 View(pocet_zaku_za_kraj)  
+#visualisation task 4 a
+pocet_zaku_za_kraj %>% ggplot(aes(Kraj, `Pocet zaku`, fill = Kraj))+
+  geom_bar(stat="identity", position="dodge")
 
 pocet_zaku_za_NUTS2 <- aggregate(neededdata$pocet_zaku ~ neededdata$NUTS2, data = neededdata, FUN = sum)
 colnames(pocet_zaku_za_NUTS2) <- c("NUTS2", "Pocet zaku")
 View(pocet_zaku_za_NUTS2)
+#visualisation task 4 b
+pocet_zaku_za_NUTS2 %>% ggplot(aes(NUTS2, `Pocet zaku`, fill = NUTS2))+
+  geom_bar(stat="identity", position="dodge")
+
+
