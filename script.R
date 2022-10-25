e69070bd858ab0adc2a7cf6111338407578b143d
irenanguyen <irenanguyen2002@seznam.cz>
2022-10-25 20:14:55 +0200

    task 4 / finished (??)

--- script.R
+++ script.R
@@ -6,6 +6,8 @@ task_one <- neededdata %>%
   group_by(kraj) %>%
   summarise(totalschool = n()) %>%
   arrange(totalschool)
+
+View(task_one)
 #task 2
 task_one$kraj <- factor(task_one$kraj, levels = c("Praha", "Středočeský", "Jihočeský", "Plzeňský", "Karlovarský", "Ústecký", "Liberecký","Královéhradecký", "Pardubický", "Vysočina","Jihomoravský", "Olomoucký", "Zlínský", "Moravskoslezský"))
 
@@ -17,8 +19,16 @@ neededdata <- mutate(neededdata, NUTS2 = case_when(neededdata$kraj == "Praha" ~ 
                                      neededdata$kraj =="Olomoucký" | neededdata$kraj =="Zlínský" ~ "Střední Morava",
                                      neededdata$kraj =="Vysočina" | neededdata$kraj =="Jihomoravský" ~ "Jihovýchod",
                                      neededdata$kraj =="Liberecký" | neededdata$kraj =="Královéhradecký" | neededdata$kraj =="Pardubický" ~ "Severovýchod",
-                                     neededdata$kraj == "Moravskoslezský" ~ "Moravskoslezsko",TRUE ~ "else"))
+                                     neededdata$kraj == "Moravskoslezský" ~ "Moravskoslezsko",TRUE ~ "N/A"))
 print(neededdata)
 View(task_one)
 
+#task 4
 
+pocet_zaku_za_kraj <- aggregate(neededdata$pocet_zaku ~ neededdata$kraj, data = neededdata, FUN = sum)
+colnames(pocet_zaku_za_kraj) <- c("Kraj", "Pocet zaku")
+View(pocet_zaku_za_kraj)  
+
+pocet_zaku_za_NUTS2 <- aggregate(neededdata$pocet_zaku ~ neededdata$NUTS2, data = neededdata, FUN = sum)
+colnames(pocet_zaku_za_NUTS2) <- c("NUTS2", "Pocet zaku")
+View(pocet_zaku_za_NUTS2)
